61caac7001ed0ba095fe86fd0c00855594e58e80
BGiraldo <bgiraldox@gmail.com>
2017-06-21 17:32:49 -0500

    End for today

---
+++ TrollGameTests/PlayerTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TrollGame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrollGame.Tests
+{
+    [TestClass()]
+    public class PlayerTests
+    {
+        [TestMethod()]
+        public void receiveDamageTest()
+        {
+           
+                Player player = new Player(50);
+                player.receiveDamage(10);
+
+                Assert.AreEqual(40, player.currentHitPoints);
+
+        }
+
+    }
+}
